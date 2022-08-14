50a507cb7a2cae724c58befd424a38bc53c1794e
fsmoledina <fsmole11@gmail.com>
2022-08-14 19:46:01 +0200

    commmit 4

--- src/MeanFilterParallel.java
+++ src/MeanFilterParallel.java
@@ -1,44 +1,163 @@
-import java.awt.image.BufferedImage;
+
 import java.io.File;
 import java.io.IOException;
-import java.lang.*;
-import java.util.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.util.concurrent.RecursiveAction;
+
+public class MeanFilterParallel extends RecursiveAction {
+    public String inputImageName, outputImageName;
+
+    protected BufferedImage bufferedImage = null;
+    protected BufferedImage bufferedMeanImage = null;
+    protected File file = null;
+
+    protected int windowWidth;
+    protected int width, height;
+
+    protected int [][] pixelValues, meanPixelValues;
+
+    protected int [] alphaRGB, sumAlphaRgb, alphaRgb;
+
+    protected int maxWorkload = 10000;
+
+    protected int x0, y0, x1, y1;
+
+    public MeanFilterParallel(int [][] pixelValues, int x0, int y0, int x1, int y1, int [][] meanPixelValues, int windowWidth) {
+        this.pixelValues = pixelValues;
+        this.meanPixelValues = meanPixelValues;
+        this.x0=x0; this.y0=y0;
+        this.x1=x1; this.y1=y1;
+        this.windowWidth=windowWidth;
+    }
+
+    protected void calculateMeans() {
+
+        for (int i=y0; i<y1; i++) {
+            for (int j=x0; j<x1; j++) {
+                meanPixelValues[i][j] = meanFunction(i, j);
+            }
+        }
+    }
+
+    protected void compute() {
+        if ( (x1-x0)*(y1-y0) > maxWorkload) {
+            //split workload
+            int halfX = (x1-x0)/2, halfY = (y1-y0)/2;
+            invokeAll(new MeanFilterParallel(pixelValues, x0,y0 halfX,halfY, meanPixelValues, windowWidth),
+                    new MeanFilterParallel(pixelValues, halfX,halfY, x1,y1, meanPixelValues, windowWidth));
+        }
+        else {
+            calculateMeans();
+        }
+    }
+
+    protected int [][] getPixels() {
+
+        //read image
+        try{
+            file = new File(inputImageName);
+            bufferedImage = ImageIO.read(file);
+        }catch(IOException e){
+            //System.out.println(e);
+            System.out.println("File no found");
+            return null;
+        }
+        width = bufferedImage.getWidth();
+        height = bufferedImage.getHeight();
+        meanPixelValues = new int [height][width];
+
+        int [][] pixels = new int [height][width];
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++) {
+                pixels[i][j] = bufferedImage.getRGB(j, i);
+            }
+        }
+        return pixels;
+    }
+
+    protected int [] getRGB(int j, int i) {
+        alphaRGB = new int [4];
+        int p = pixelValues[i][j];
+        alphaRGB[0] = (p>>24) & 0xff;
+        alphaRGB[1] = (p>>16) & 0xff;
+        alphaRGB[2] = (p>>8) & 0xff;
+        alphaRGB[3] = p & 0xff;
+        return alphaRGB;
+    }
+
+    protected int meanFunction(int i, int j) {
+        int meanRadius = (windowWidth-1)/2;
+        sumAlphaRgb = new int [4];
+
+        int count=0;
+        for (int k = i-meanRadius; k <= i+meanRadius; k++) {
+            for (int m=j-meanRadius; m<=j-meanRadius; m++) {
+                if ((m>=0 && m<width) && (k>=0 && k<height)) {
+                    alphaRgb = getRGB(m, k);
+                    sumAlphaRgb[0] += alphaRgb[0];
+                    sumAlphaRgb[1] += alphaRgb[1];
+                    sumAlphaRgb[2] += alphaRgb[2];
+                    sumAlphaRgb[3] += alphaRgb[3];
+                    count++;
+                }
+            }
+        }
+
+        // average
+        sumAlphaRgb[0] = (int)Math.round((sumAlphaRgb[0]*1.0)/count);
+        sumAlphaRgb[1] = (int)Math.round((sumAlphaRgb[1]*1.0)/count);
+        sumAlphaRgb[2] = (int)Math.round((sumAlphaRgb[2]*1.0)/count);
+        sumAlphaRgb[3] = (int)Math.round((sumAlphaRgb[3]*1.0)/count);
+
+        int p = (sumAlphaRgb[0]<<24) | (sumAlphaRgb[1]<<16) | (sumAlphaRgb[2]<<8) | sumAlphaRgb[3];
+
+        return p;
+    }
+
+    protected static boolean outputImage() {
+        bufferedMeanImage = new BufferedImage(width, height, bufferedImage.getType());
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++) {
+                bufferedMeanImage.setRGB(j, i, meanPixelValues[i][j]);
+            }
+        }
+
+        try{
+            file = new File(outputImageName);
+            return ImageIO.write(bufferedMeanImage, "jpeg", file);
+        }catch(IOException e){
+            // System.out.println(e);
+            System.out.println("Unable to write to file");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        inputImageName  = args[0];
+        outputImageName = args[1];
+        windowWidth     = Integer.parseInt(args[2]);
+
+        // MeanFilterParallel forFilesIO = new MeanFilterParallel(inputImageName, outputImageName, windowWidth);
+
+        int [][] pixelValues = forFilesIO.getPixels();
+        int [][] meanPixelValues = forFilesIO.meanPixelValues;
+        int width=forFilesIO.width, height=forFilesIO.height;
+
+        MeanFilterParallel filter = new MeanFilterParallel(pixelValues, 0,0, width,height, meanPixelValues, windowWidth);
+
+        ForkJoinPool pool = new ForkJoinPool();
 
 
-public class MeanFilterParallel {
+        long initial = System.currentTimeMillis();
+        pool.invoke(filter);
+        long difference = System.currentTimeMillis() - initial;
 
-    public static void main(String[] args)throws IOException {
-        BufferedImage img = null;
-        File f = null;
+        System.out.println("Time lapsed is "+difference+" milliseconds");
 
-        //get image width and height
-        int width = img.getWidth();
-        int height = img.getHeight();
+        // outputImage(meanPixelValues, );
 
-        //get pixel value
-        int p = img.getRGB(0,0);
-
-        //get alpha
-        int a = (p>>24) & 0xff;
-
-        //get red
-        int r = (p>>16) & 0xff;
-
-        //get green
-        int g = (p>>8) & 0xff;
-
-        //get blue
-        int b = p & 0xff;
-
-        a = 255;
-        r = 100;
-        g = 150;
-        b = 200;
-
-        //set the pixel value
-        p = (a<<24) | (r<<16) | (g<<8) | b;
-        img.setRGB(0, 0, p);
-
-
-    }//main() ends here
-}//class ends here
+        // open image, save pix as int [][]
+    }
+}
--- src/MeanFilterSerial.java
+++ src/MeanFilterSerial.java
@@ -1,85 +1,128 @@
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+
 import java.io.File;
 import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class MeanFilterSerial {
-    public static void main(String[] args)throws IOException{
-        BufferedImage img = null;
-        File f = null;
+    static String inputImageName, outputImageName;
 
-        //get image width and height
-        int width = img.getWidth();
-        int height = img.getHeight();
+    static BufferedImage bufferedImage = null;
+    static BufferedImage bufferedMeanImage = null;
+    static File file = null;
 
-        //get pixel value
-        int p = img.getRGB(0,0);
+    static int windowWidth;
+    static int width, height;
 
-        //get alpha
-        int a = (p>>24) & 0xff;
+    static int [][] pixelValues, meanPixelValues;
 
-        //get red
-        int r = (p>>16) & 0xff;
+    static int [] alphaRGB, sumAlphaRgb, alphaRgb;
 
-        //get green
-        int g = (p>>8) & 0xff;
+    static void calculateMeans() {
+        meanPixelValues = new int [height][width];
 
-        //get blue
-        int b = p & 0xff;
-
-        a = 255;
-        r = 100;
-        g = 150;
-        b = 200;
-
-        //set the pixel value
-        p = (a<<24) | (r<<16) | (g<<8) | b;
-        img.setRGB(0, 0, p);
-
-
-    }//main() ends here
-    public class ForkBlur extends RecursiveAction {
-        private int[] mSource;
-        private int mStart;
-        private int mLength;
-        private int[] mDestination;
-
-        // Processing window size; should be odd.
-        private int mBlurWidth = 15;
-
-        public ForkBlur(int[] src, int start, int length, int[] dst) {
-            mSource = src;
-            mStart = start;
-            mLength = length;
-            mDestination = dst;
-        }
-
-        protected void computeDirectly() {
-            int sidePixels = (mBlurWidth - 1) / 2;
-            for (int index = mStart; index < mStart + mLength; index++) {
-                // Calculate average.
-                float rt = 0, gt = 0, bt = 0;
-                for (int mi = -sidePixels; mi <= sidePixels; mi++) {
-                    int mindex = Math.min(Math.max(mi + index, 0),
-                            mSource.length - 1);
-                    int pixel = mSource[mindex];
-                    rt += (float)((pixel & 0x00ff0000) >> 16)
-                            / mBlurWidth;
-                    gt += (float)((pixel & 0x0000ff00) >>  8)
-                            / mBlurWidth;
-                    bt += (float)((pixel & 0x000000ff) >>  0)
-                            / mBlurWidth;
-                }
-
-                // Reassemble destination pixel.
-                int dpixel = (0xff000000     ) |
-                        (((int)rt) << 16) |
-                        (((int)gt) <<  8) |
-                        (((int)bt) <<  0);
-                mDestination[index] = dpixel;
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++) {
+                meanPixelValues[i][j] = meanFunction(i, j);
             }
         }
-}//class ends here
+    }
 
+    static int [][] getPixels() {
 
+        //read image
+        try{
+            file = new File(inputImageName);
+            bufferedImage = ImageIO.read(file);
+        }catch(IOException e){
+            //System.out.println(e);
+            System.out.println("File no found");
+            return null;
+        }
+        width = bufferedImage.getWidth();
+        height = bufferedImage.getHeight();
 
+        int [][] pixels = new int [height][width];
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++) {
+                pixels[i][j] = bufferedImage.getRGB(j, i);
+            }
+        }
+        return pixels;
+    }
+
+    static int [] getRGB(int j, int i) {
+        alphaRGB = new int [4];
+        int p = pixelValues[i][j];
+        alphaRGB[0] = (p>>24) & 0xff;
+        alphaRGB[1] = (p>>16) & 0xff;
+        alphaRGB[2] = (p>>8) & 0xff;
+        alphaRGB[3] = p & 0xff;
+        return alphaRGB;
+    }
+
+    static int meanFunction(int i, int j) {
+        int meanRadius = (windowWidth-1)/2;
+        sumAlphaRgb = new int [4];
+
+        int count=0;
+        for (int k = i-meanRadius; k <= i+meanRadius; k++) {
+            for (int m=j-meanRadius; m<=j-meanRadius; m++) {
+                if ((m>=0 && m<width) && (k>=0 && k<height)) {
+                    alphaRgb = getRGB(m, k);
+                    sumAlphaRgb[0] += alphaRgb[0];
+                    sumAlphaRgb[1] += alphaRgb[1];
+                    sumAlphaRgb[2] += alphaRgb[2];
+                    sumAlphaRgb[3] += alphaRgb[3];
+                    count++;
+                }
+            }
+        }
+
+        // average
+        sumAlphaRgb[0] = (int)Math.round((sumAlphaRgb[0]*1.0)/count);
+        sumAlphaRgb[1] = (int)Math.round((sumAlphaRgb[1]*1.0)/count);
+        sumAlphaRgb[2] = (int)Math.round((sumAlphaRgb[2]*1.0)/count);
+        sumAlphaRgb[3] = (int)Math.round((sumAlphaRgb[3]*1.0)/count);
+
+        int p = (sumAlphaRgb[0]<<24) | (sumAlphaRgb[1]<<16) | (sumAlphaRgb[2]<<8) | sumAlphaRgb[3];
+
+        return p;
+    }
+
+    static boolean outputImage() {
+        bufferedMeanImage = new BufferedImage(width, height, bufferedImage.getType());
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++) {
+                bufferedMeanImage.setRGB(j, i, meanPixelValues[i][j]);
+            }
+        }
+
+        try{
+            file = new File(outputImageName);
+            return ImageIO.write(bufferedMeanImage, "jpeg", file);
+        }catch(IOException e){
+            // System.out.println(e);
+            System.out.println("Unable to write to file");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        inputImageName  = args[0];
+        outputImageName = args[1];
+        windowWidth     = Integer.parseInt(args[2]);
+
+        pixelValues = getPixels();
+
+        long initial = System.currentTimeMillis();
+        calculateMeans();
+        long difference = System.currentTimeMillis() - initial;
+
+        System.out.println("Time lapsed is "+difference+" milliseconds");
+
+        outputImage();
+
+        // open image, save pix as int [][]
+    }
+}
